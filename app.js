305249ba17049fc186b0db2626e103e339d0f9ff
Victor <nguyentanthang9a4@gmail.com>
2020-05-13 11:17:45 +0700

    Version 1.0.0 - Complete basic functions

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const resolvers = require("./resolver/resolver");
 require("dotenv").config({path:"./config/config.env"});
 require("./config/db")();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, engine: {
+  apiKey: "service:cms-graph:L9yUKrF2CSzILFpz1n7pyg",
+} });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
